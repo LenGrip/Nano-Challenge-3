57f9b34d919c115884e6f829d3c1d973e2a21967
LenGrip <102364354+LenGrip@users.noreply.github.com>
2024-07-08 16:21:40 +0700

    Initial Commit

---
+++ Mini-Challenge-3/Mini-Challenge-3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mini-Challenge-3
+//
+//  Created by Jayson Adrian Sunaryo on 08/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mini-Challenge-3/Mini-Challenge-3/Mini_Challenge_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_Challenge_3App.swift
+//  Mini-Challenge-3
+//
+//  Created by Jayson Adrian Sunaryo on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Mini_Challenge_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
